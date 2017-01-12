f019e510ec7118c68e5d8a2e200b30dfed1b4d0e
Livio Asdrubal <asdrubal.livio@gmail.com>
2017-01-12 15:51:33 +0100

    Javascript Project + mini server rest api

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# dta-javascript
+
+## Préparation
+
+Dans un shell
+
+```
+>npm init
+>npm install json-server --save-dev
+>npm install serve --save-dev
+```
+
+## Lancement
+
+### Lancement api json
+
+Dans un premier shell
+
+```
+>npm run api
+```
+
+### Lancement de l'application
+
+Dans un second shell
+
+```
+>npm start
+```# dta-javascript
---
+++ javascript/pizayolo.js
@@ -0,0 +1,72 @@
+class RecipesService {
+    constructor() {
+
+    }
+
+    getRecipes() {
+
+        return fetch('http://localhost:3000/recipes')
+            .then(response => response.json());
+
+    }
+
+    isRecipeCompliant(recipe, pizza) {
+        if (pizza.length !== recipe.length) {
+            return false;
+        }
+
+        let array = [];
+        let array2 = [];
+        pizza.forEach(function(item) {
+            let contenu = recipe.filter(function(items) {
+                return item === items;
+            });
+            if (contenu.length != 0) {
+                array.push(contenu)
+            }
+        });
+
+        if (array.length !== recipe.length) {
+            return false;
+        } else {
+            recipe.forEach(function(item) {
+                let contenu = pizza.filter(function(items) {
+                    return item === items;
+                });
+                if (contenu.length != 0) {
+                    array2.push(contenu)
+                }
+            });
+            if (array2.length !== recipe.length) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+    }
+
+    getRecipe(name) {
+
+        return this.getRecipes()
+            .then(recipes => recipes.find(recipe => recipe.name === name));
+
+    }
+
+    getRecipesNames() {
+
+        return this.getRecipes()
+            .then(recipes => recipes.map(recipe => recipe.name))
+
+    }
+
+    queryRecipes(query) {
+        return this.getRecipes()
+            .then(recipes =>
+                recipes
+                .filter(recipe =>
+                    recipe.name.toLowerCase().includes(query.toLowerCase())
+                )
+            );
+    }
+
+}
